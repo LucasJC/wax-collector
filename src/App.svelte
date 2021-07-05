30b03a050215e9a96a688a4f950a6b133f0746da
LucasJC <lucas.couchot@gmail.com>
2021-07-05 01:26:41 -0300

    Add google analytics tag

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import { Router, Route } from "svelte-navigator";
+	import { GoogleAnalytics } from "@beyonk/svelte-google-analytics";
 	import ComparatorPage from "./components/ComparatorPage/index.svelte";
 	import Header from "./components/Header.svelte";
 	import Footer from "./components/Footer.svelte";
 </script>
 
+<GoogleAnalytics properties={["G-7RZDJEMHFR"]} />
 <div class="container">
 	<Router>
 		<Header />
